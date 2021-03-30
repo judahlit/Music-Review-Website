4fd64033a42e31b0c843f03d895fceb3a11da338
Judah <judahlit03@gmail.com>
2021-03-30 02:01:13 +0200

    Song is now submittable, error message can now be received

--- Music Review Application GUI/Models/SongModel.cs
+++ Music Review Application GUI/Models/SongModel.cs
@@ -9,8 +9,6 @@ namespace Music_Review_Application_GUI.Models
 {
     public class SongModel
     {
-        #region Properties
-
         public int SongId { get; private set; }
         public int AlbumId { get; set; }
         public string Title { get; set; }
@@ -19,44 +17,5 @@ namespace Music_Review_Application_GUI.Models
         public string DateMonth { get; set; }
         public string DateYear { get; set; }
         public DateTime Date { get; set; }
-        public string Genre { get; set; }
-
-        #endregion
-
-        #region Constructor
-
-        public SongModel(string title, string artist, string dateDay, string dateMonth, string dateYear)
-        {
-            Title = title;
-            Artist = artist;
-            DateDay = dateDay;
-            DateMonth = dateMonth;
-            DateYear = dateYear;
-        }
-
-        #endregion
-
-        #region Methods
-
-        public void AddSingleToDB()
-        {
-            Song song = new Song(Title, Artist, 0, Date);
-        }
-
-        public void ToDateTime()
-        {
-            DateTime correctDate;
-
-            if (DateTime.TryParse(DateYear + "-" + DateMonth + "-" + DateDay, out correctDate))
-            {
-                Date = correctDate;
-            }
-            else
-            {
-                AddSongModel.ErrorMessage = "Please fill in a valid date in the 'Date of Release' field.";
-            }
-        }
-
-        #endregion
     }
 }
--- Music Review Application GUI/Pages/Forms/AddSong.cshtml.cs
+++ Music Review Application GUI/Pages/Forms/AddSong.cshtml.cs
@@ -5,33 +5,59 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Music_Review_Application_GUI.Models;
+using Music_Review_Application_LIB;
 
 namespace Music_Review_Application_GUI.Pages.Forms
 {
     public class AddSongModel : PageModel
     {
+
         [BindProperty]
         public SongModel Song { get; set; }
-
         public static string ErrorMessage { get; set; }
 
         public void OnGet()
         {
-            if (ErrorMessage != null)
-            {
-                // TODO: Show error message
-                ErrorMessage = null;
-            }
+            
         }
 
         public IActionResult OnPost()
         {
-            if (ErrorMessage == null)
+            ErrorMessage = null;
+
+            if (ToDateTime())
             {
-                return RedirectToPage("/Index");
+                AddSingleToDB();
+            }
+            else
+            {
+                ErrorMessage = "Please fill in a valid date in the 'Date of Release' field.";
             }
 
-            return Page();
+            if (ErrorMessage != null)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("/Index");
+        }
+
+        public void AddSingleToDB()
+        {
+            Song song = new Song(Song.Title, Song.Artist, 0, Song.Date);
+        }
+
+        public bool ToDateTime()
+        {
+            DateTime correctDate;
+
+            if (DateTime.TryParse($"{Song.DateYear}-{Song.DateMonth}-{Song.DateDay}", out correctDate) && DateTime.Compare(correctDate, DateTime.Now) <= 0)
+            {
+                Song.Date = correctDate;
+                return true;
+            }
+
+            return false;
         }
     }
 }
