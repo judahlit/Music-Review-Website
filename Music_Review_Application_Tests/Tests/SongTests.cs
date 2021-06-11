0ce0473086dd5fa34d69bb42467ddd8bf0ea3dcf
Judah <judahlit03@gmail.com>
2021-06-11 10:09:51 +0200

    Added another test.

--- Music_Review_Application_Tests/Tests/SongTests.cs
+++ Music_Review_Application_Tests/Tests/SongTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Autofac;
 using Music_Review_Application_DB_Managers.Interfaces;
 using Xunit;
@@ -29,6 +30,33 @@ namespace Music_Review_Application_Integration_Tests.Tests
 
             // Assert
             Assert.True(songId > 0);
+        }
+
+        [Fact]
+        public void GetSongIdReturnsZeroWhenSearchingNonExistentArtists()
+        {
+            var song = SampleData.GetSampleSingle();
+
+            // Arrange
+            var songId = 0;
+            var nonExistingArtists = new List<string> {"jsoiapfjdiosjiop", "doafjdsopj"};
+
+            var container = TestContainerConfig.Configure();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var songDbManager = scope.Resolve<ISongDbManager>();
+                songDbManager.AddSingle(song);
+                var song2 = song;
+                song2.ArtistNames = nonExistingArtists;
+
+                // Act
+                songId = songDbManager.GetSongId(song2.Title, song2.ArtistNames);
+
+                songDbManager.DeleteSingle(songDbManager.GetSongId(song.Title, song.ArtistNames));
+            }
+
+            // Assert
+            Assert.Equal(0, songId);
         }
 
         [Fact]
